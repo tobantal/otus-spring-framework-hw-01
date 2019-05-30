21c418dbfc89f507e6facb06552675d83451ee77
Anton Tobolkin <tobantal@gmail.com>
2019-05-31 00:20:34 +0700

    add .gitcommit

--- src/main/java/ru/otus/spring/hw01/controller/Examinator.java
+++ src/main/java/ru/otus/spring/hw01/controller/Examinator.java
@@ -1,5 +1,6 @@
 package ru.otus.spring.hw01.controller;
 
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -8,6 +9,10 @@ import ru.otus.spring.hw01.domain.Task;
 
 // move to service
 public class Examinator implements Supplier<Task>, Consumer<Answer> {
+	
+	private List<Task> tasks;
+	
+	
 
 	@Override
 	public void accept(Answer t) {

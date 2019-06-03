c0127db18f94d4c331a124446a72ea7a520259ab
Тоболкин Антон Александрович <aatobolkin@ois.ru>
2019-06-03 12:25:56 +0700

    add junit5 test

---
+++ src/test/java/ru/otus/spring/hw01/dao/CsvDaoTest.java
@@ -0,0 +1,43 @@
+package ru.otus.spring.hw01.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Queue;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+import ru.otus.spring.hw01.domain.Task;
+
+
+class CsvDaoTest {
+	
+	private TaskDao taskDao;
+	
+	/*
+	public CsvDaoTest(TaskDao taskDao) {
+		super();
+		this.taskDao = taskDao;
+	}
+	*/
+
+	public void setTaskDao(TaskDao taskDao) {
+		this.taskDao = taskDao;
+	}
+
+	@Test
+	void test() {
+		
+		TaskDao taskDao = new CsvDao(); // new CsvDao("test-tasks.csv");
+		CsvDao csvDao = (CsvDao) taskDao;
+		String csvPath = "test-tasks.csv";
+		csvDao.setCsvPath(csvPath);
+		
+		
+		Queue<Task> queue = taskDao.getQueueTasks();
+		System.out.println(queue.peek().getText());
+		
+		assertTrue(IntStream.rangeClosed(1, 5).allMatch(x -> queue.poll().getId() == x));
+		assertNull(queue.poll());
+	}
+
+}

c6ae2c2334bee276e0bee1bf5832c12269c40ef8
Тоболкин Антон Александрович <aatobolkin@ois.ru>
2019-06-04 12:23:34 +0700

    add new test

--- src/main/java/ru/otus/spring/hw01/dao/CsvDao.java
+++ src/main/java/ru/otus/spring/hw01/dao/CsvDao.java
@@ -22,9 +22,6 @@ public class CsvDao implements TaskDao {
 		try (Scanner scanner = new Scanner(this.getClass().getClassLoader().getResourceAsStream(csvPath));) {
 		    while (scanner.hasNextLine()) {
 		        String[] line = scanner.nextLine().split(DELIMITER);
-		        //Long id = Long.parseLong(line[0]);
-		        //String text = line[1].substring(1, line[1].length()-1);
-		        //String ans = line[2].substring(1, line[2].length()-1);
 		        queue.add(new Task(Long.parseLong(line[0]), line[1], line[2]));
 		    }
 		}
--- src/main/java/ru/otus/spring/hw01/domain/Answer.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.spring.hw01.domain;
-
-public class Answer {
-	
-	//private long id;
-	//private String userAnswer;
-
-}
